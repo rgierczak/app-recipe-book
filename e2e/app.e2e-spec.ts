6eca6b0f0ee129d76c2b5c9e3036e48c607406dd
Rafał Gierczak <grime85@gmail.com>
2016-12-13 15:55:16 +0100

    Change indent size to 4

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { RecipeBookPage } from './app.po';
 
-describe('recipe-book App', function() {
-  let page: RecipeBookPage;
+describe('recipe-book App', function () {
+    let page: RecipeBookPage;
 
-  beforeEach(() => {
-    page = new RecipeBookPage();
-  });
+    beforeEach(() => {
+        page = new RecipeBookPage();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('rb works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('rb works!');
+    });
 });
