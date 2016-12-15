13bdc64dd6cdacc17f65f7922b5f2592b0c48e91
Rafał Gierczak <grime85@gmail.com>
2016-12-15 19:45:40 +0100

    Creating the Recipe Form with the Data-Driven Approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -34,6 +34,7 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
     imports: [
         BrowserModule,
         FormsModule,
+        ReactiveFormsModule,
         HttpModule,
         routing
     ],
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-import { RecipeService } from "../recipe.service";
+import {
+    FormArray,
+    FormGroup,
+    FormControl,
+    FormBuilder,
+    Validators
+} from "@angular/forms";
+
 import { Subscription } from "rxjs/Rx";
+import { RecipeService } from "../recipe.service";
+import { Recipe } from "../recipe";
 
 @Component({
     selector: 'rb-recipe-edit',
     templateUrl: './recipe-edit.component.html'
 })
 export class RecipeEditComponent implements OnInit, OnDestroy {
+    recipeForm: FormGroup;
     private recipeIndex: number;
+    private recipe: Recipe;
+    private isNew = true;
     private subscription: Subscription;
 
     constructor(private route: ActivatedRoute,
-                private recipeService: RecipeService) {
+                private recipeService: RecipeService,
+                private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
-        let isNew = true;
         this.subscription = this.route.params.subscribe(
             (params: any) => {
                 if (params.hasOwnProperty('id')) {
-                    isNew = false;
+                    this.isNew = false;
                     this.recipeIndex = Number(params['id']);
+                    this.recipe = this.recipeService.getRecipe(this.recipeIndex);
                 } else {
-                    isNew = true;
+                    this.isNew = true;
+                    this.recipe = null;
                 }
             }
         );
@@ -31,5 +45,37 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         this.subscription.unsubscribe();
+    }
+
+    private initForm(isNew: boolean) {
+        let recipeName = '';
+        let recipeImageUrl = '';
+        let recipeContent = '';
+        let recipeIngredients: FormArray = new FormArray([]);
+
+        if (!this.isNew) {
+            for (let i = 0; i < this.recipe.ingrediens.length; i++) {
+                recipeIngredients.push(
+                    new FormGroup({
+                        name: new FormControl(this.recipe.ingrediens[i].name, Validators.required),
+                        amount: new FormControl(this.recipe.ingrediens[i].amount, [
+                            Validators.required,
+                            Validators.pattern("\\d+")
+                        ])
+                    })
+                );
+            }
+
+            recipeName = this.recipe.name;
+            recipeImageUrl = this.recipe.imagePath;
+            recipeContent = this.recipe.description;
+        }
+
+        this.recipeForm = this.formBuilder.group({
+            name: [recipeName, Validators.required],
+            imagePath: [recipeImageUrl, Validators.required],
+            description: [recipeContent, Validators.required],
+            ingredients: recipeIngredients
+        });
     }
 }
