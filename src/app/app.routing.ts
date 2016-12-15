02f8f4b0391290bca98eb1876e8f3552bfe11e83
Rafał Gierczak <grime85@gmail.com>
2016-12-15 09:22:00 +0100

    Setting up the Main Routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from "@angular/router";
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const APP_ROUTES: Routes = [
+    {
+        path: '',
+        redirectTo: '/recipes',
+        pathMatch: 'full'
+    },
+    {
+        path: 'recipes',
+        component: RecipesComponent
+    },
+    {
+        path: 'shopping-list',
+        component: ShoppingListComponent
+    }
+
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
