b2c924ebe73940e0e95e42047a61e45d336a0e13
Rafał Gierczak <grime85@gmail.com>
2016-12-15 16:07:22 +0100

    Submitting the Shopping List Form

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient";
+import { ShoppingListService } from "./shopping-list.service";
 
 @Component({
     selector: 'rb-shopping-list-add',
     templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+    item: Ingredient;
     isAdd = true;
 
-    constructor() {
+    constructor(private shoppingListService: ShoppingListService) {}
 
-    }
+    ngOnInit() {}
 
-    ngOnInit() {
-
+    onSubmit(ingredient: Ingredient) {
+        if (!this.isAdd) {
+            // Edit
+        } else {
+            this.item = new Ingredient(ingredient.name, ingredient.amount);
+            this.shoppingListService.addItem(this.item);
+        }
     }
 }
